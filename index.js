1b83ca934723782f22747fc68937471a9220bd1f
jransome <jamesransome@msn.com>
2020-03-28 08:23:38 +0000

    Input -> action

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-// module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
+const { Engine, Events, Render, World, Body, Bodies, Mouse, MouseConstraint } = Matter
 
-// create an engine
-var engine = Engine.create();
-
-// create a renderer
-var render = Render.create({
-    element: document.body,
-    engine: engine
+const engine = Engine.create();
+const render = Render.create({
+  engine,
+  element: document.body,
 });
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+// mouse stuff
+const mouse = Mouse.create(document.body);
+const mouseConstraint = MouseConstraint.create(engine, { mouse })
+World.add(engine.world, mouseConstraint);
+render.mouse = mouse; // keep the mouse in sync with rendering
 
-// add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
 
-// run the engine
+const bounds = [
+  Bodies.rectangle(400, 610, 810, 60, { isStatic: true }),
+  Bodies.rectangle(400, -10, 810, 60, { isStatic: true }),
+  Bodies.rectangle(-10, 400, 810, 60, { isStatic: true, angle: 0.5 * Math.PI }),
+  Bodies.rectangle(810, 400, 810, 60, { isStatic: true, angle: 0.5 * Math.PI }),
+]
+
+const boxA = Bodies.rectangle(300, 150, 80, 100);
+const boxB = Bodies.rectangle(350, 190, 80, 80);
+World.add(engine.world, [boxA, boxB, ...bounds]);
+
+engine.world.gravity = { x: 0, y: 0 }
+
+
+
+Events.on(mouseConstraint, 'mousedown', function (event) {
+  const mousePosition = event.mouse.position;
+  console.log('mousedown at ' + mousePosition.x + ' ' + mousePosition.y);
+  Body.applyForce(boxA, mousePosition, { x: 0.05, y: 0 })
+});
+
 Engine.run(engine);
-
-// run the renderer
 Render.run(render);
