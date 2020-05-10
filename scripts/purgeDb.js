2706b136cf3c0bd7634816579dfc9c0105b72f50
jransome <jamesransome@msn.com>
2020-05-10 20:03:07 +0100

    Add purge script

---
+++ scripts/purgeDb.js
@@ -0,0 +1,10 @@
+const { GameCollection, PlayerCollection } = require('../src/database');
+
+const deleteAll = (collection) => collection
+  .listDocuments()
+  .then(docs => Promise.all(docs.map(doc => doc.delete())))
+  .then(() => console.log(collection.path, 'purged'))
+  .catch((e) => console.error(`Failed to purge ${collection.path}:`, e));
+
+deleteAll(GameCollection);
+deleteAll(PlayerCollection);
--- src/database.js
+++ src/database.js
@@ -3,37 +3,10 @@ const serviceAccount = require('../firebaseSecrets.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://multiplayer-pool.firebaseio.com'
+  databaseURL: 'https://multiplayer-pool.firebaseio.com',
 });
 
 const db = admin.firestore();
-
-// const playerCollection = db.collection('players');
-
-// playerCollection.get().then(d => console.log('got data!', d.docs[0].data()))
-
-
-//  Player.createIfNotExists('Picard').then(j => {
-// console.log(j)
-//    setTimeout(() => j.logout(), 8000);
-//  })
-
-
-// const createIfNotExists = async (playerName) => {
-//   const playerDocumentReference = playerCollection.doc(playerName);
-//   if ((await playerDocumentReference.get()).exists) return playerDocumentReference;
-
-//   await playerDocumentReference.create({
-//     name: playerName,
-//     gamesStarted: 0,
-//     gamesAbandoned: 0,
-//     gamesWon: 0,
-//     timePlayedSecs: 0,
-//   }).catch(e => console.error('Error on player document creation:', e));
-//   return playerDocumentReference;
-// }
-
-
 
 module.exports = {
   GameCollection: db.collection('games'),
