abf17042675e45a1bacf6382ebaa8f8fe752da67
jransome <jamesransome@msn.com>
2020-05-11 11:00:57 +0100

    Fix db env vars

--- src/database.js
+++ src/database.js
@@ -1,7 +1,20 @@
 const admin = require('firebase-admin');
 
+const serviceAccount = {
+  type: "service_account",
+  project_id: "multiplayer-pool",
+  private_key_id: process.env.DB_PRIVATE_KEY_ID,
+  private_key: process.env.DB_PRIVATE_KEY,
+  client_email: process.env.DB_CLIENT_EMAIL,
+  client_id: process.env.DB_CLIENT_ID,
+  auth_uri: process.env.DB_AUTH_URI,
+  token_uri: process.env.DB_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.DB_AUTH_PROVIDER_URL,
+  client_x509_cert_url: process.env.DB_CLIENT_CERT_URL,
+}
+
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://multiplayer-pool.firebaseio.com',
 });
 
