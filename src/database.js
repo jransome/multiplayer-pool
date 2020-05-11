3e916b2c0b78138d795052f3e216a986e4f55f00
jransome <jamesransome@msn.com>
2020-05-11 18:38:56 +0100

    Parse firebase key

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const serviceAccount = {
   type: "service_account",
   project_id: "multiplayer-pool",
   private_key_id: process.env.DB_PRIVATE_KEY_ID,
-  private_key: process.env.DB_PRIVATE_KEY,
+  private_key: process.env.DB_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.DB_CLIENT_EMAIL,
   client_id: process.env.DB_CLIENT_ID,
   auth_uri: process.env.DB_AUTH_URI,
