9a79bd81380e47992bbed775e37c01ea685081ee
James Ransome <jamesransome@msn.com>
2020-09-23 10:49:44 +0100

    Add nice song

--- src/client/components/Songs.js
+++ src/client/components/Songs.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import niceSong from '../nice.mp3';
 import tropicalSong from '../tropical.mp3';
 
 const Songs = () => (
   <div style={{ marginLeft: '420px', marginTop: '20px' }}>
     <strong>Play some music!</strong>
+    <br />
+    <audio controls src={niceSong} />
     <br />
     <audio controls src={tropicalSong} />
   </div>
