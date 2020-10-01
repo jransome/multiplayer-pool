9e23ff5b3d71e31764175fe9487fc89c2ad5d7b4
James Ransome <jamesransome@msn.com>
2020-10-01 10:43:36 +0100

    Add irish song

--- src/client/components/Songs.js
+++ src/client/components/Songs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import niceSong from '../nice.mp3';
 import tropicalSong from '../tropical.mp3';
+import irishSong from '../irish.mp3';
 
 const Songs = () => (
   <div style={{ marginLeft: '420px', marginTop: '20px' }}>
@@ -9,6 +10,8 @@ const Songs = () => (
     <audio controls src={niceSong} />
     <br />
     <audio controls src={tropicalSong} />
+    <br />
+    <audio controls src={irishSong} />
   </div>
 );
 
