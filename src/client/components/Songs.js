a954b84b975b7154f2b7601b3bd067108f236a3d
James Ransome <jamesransome@msn.com>
2021-06-20 15:06:55 +0100

    Change songs

--- src/client/components/Songs.js
+++ src/client/components/Songs.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import niceSong from '../nice.mp3';
-import tropicalSong from '../tropical.mp3';
-import irishSong from '../irish.mp3';
+import oneFourSix from '../songs/I-IV-VI.m4a';
+import pianoSong from '../songs/Piano.m4a';
+import retroSong from '../songs/RetroDemoPlanning.m4a';
+import tropicalSong from '../songs/tropical.mp3';
 
 const Songs = () => (
   <div style={{ marginLeft: '420px', marginTop: '20px' }}>
     <strong>Play some music!</strong>
-    <br />
-    <audio controls src={irishSong} />
-    <br />
-    <audio controls src={tropicalSong} />
-    <br />
-    <audio controls src={niceSong} />
+    <br /> 80s <audio controls src={retroSong} />
+    <br /> Tropical <audio controls src={tropicalSong} />
+    <br /> Piano <audio controls src={pianoSong} />
+    <br /> Guitar <audio controls src={oneFourSix} />
   </div>
 );
 
